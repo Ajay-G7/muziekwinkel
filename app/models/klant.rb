3122af59dd443963a4153d24c4e66d2af9eb00c3
Hugo Arts <hugo.yoshi@gmail.com>
2010-01-13 14:09:49 +0100

    validate email adresses in model

--- app/models/klant.rb
+++ app/models/klant.rb
@@ -1,5 +1,8 @@
 class Klant < ActiveRecord::Base
   validates_presence_of :voornaam, :achternaam, :email, :creditcard, :postcode, :huisnummer
+  validates_uniqueness_of :email, :creditcard
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+
   has_many :aankoops
   has_many :commentaars
 
--- app/models/medewerker.rb
+++ app/models/medewerker.rb
@@ -1,3 +1,5 @@
 class Medewerker < ActiveRecord::Base
+  validates_uniqueness_of :email
   validates_presence_of :email, :password
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
